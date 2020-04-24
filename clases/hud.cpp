2138b2dd68bbff232c2dbf8b4c74311b6fbefd81
Fran Luque <franluque2@gmail.com>
2020-04-24 20:15:43 -0300

    Arreglado problema con el panel derecho.

--- clases/hud.cpp
+++ clases/hud.cpp
@@ -7,13 +7,13 @@ Texture2D hudTexturePanel;
 Rectangle hudRightPanel;
 void hudInit()
 {
-    hudRightPanel={static_cast<float>(GetScreenWidth()-(GetScreenWidth()/5)),static_cast<float>(GetScreenHeight()/2),static_cast<float>(GetScreenWidth()/5), static_cast<float>(GetScreenHeight())};
+    hudRightPanel={static_cast<float>(GetScreenWidth()-(GetScreenWidth()/5)),0,static_cast<float>(GetScreenWidth()/5*2), static_cast<float>(GetScreenHeight())};
     hudTexturePanel = LoadTexture("resources/HudPanelRight.png");
 
 }
 void hudDraw()
 {
-    DrawTextureRec(hudTexturePanel, hudRightPanel, Vector2{ static_cast<float>(GetScreenWidth()-(GetScreenWidth()/5)),static_cast<float>(GetScreenHeight()/2) } ,WHITE);
+    DrawTexture(hudTexturePanel, static_cast<float>(GetScreenWidth()-(GetScreenWidth()/5)*1.54),0 ,WHITE);
 }
 
 void hudUnload()
